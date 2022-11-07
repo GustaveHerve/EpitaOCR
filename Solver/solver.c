30aaf94c3e50eee407a682ed516bd49d879dd716
Leo sambrook <leo.sambrook@epita.fr>
2022-11-07 18:25:58 +0100

    Fixed solver

--- Solver/solver.c
+++ Solver/solver.c
@@ -3,9 +3,9 @@
 #include <ctype.h>
 #define N 9
 
-//This function creates a new file 
+//This function creates a new file
 //with the solved sudoku on it
-void print(int arr[N][N])
+void print(long arr[N][N])
 {
 
 	FILE * fp;
@@ -18,50 +18,51 @@ void print(int arr[N][N])
 	 }
 	 fprintf(fp, "%s","\n");
        }
+     fclose(fp);
 }
 
 // function used to check if it's safe
 // to place a value (backtracking method)
 
-int isSafe(int grid[N][N], int row,
-                       int col, int num)
+int isSafe(long grid[N][N], long row,
+                       long col, long num)
 {
-     
+
     // Check if we find the same num
     // in the similar row , we return 0
     for (int x = 0; x <= 8; x++)
         if (grid[row][x] == num)
             return 0;
- 
+
     // Check if we find the same num in the
     // similar column , we return 0
     for (int x = 0; x <= 8; x++)
         if (grid[x][col] == num)
             return 0;
- 
+
     // Check if we find the same num in the
     // particular 3*3 matrix, we return 0
     int startRow = row - row % 3,
                  startCol = col - col % 3;
-   
+
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             if (grid[i + startRow][j +
                           startCol] == num)
                 return 0;
- 
+
     return 1;
 }
- 
+
 /* main sudoku function that solves using the backtracking method */
-int solveSudoku(int grid[N][N], int row, int col)
+int solveSudoku(long grid[N][N], long row, long col)
 {
-     
+
     // Check if we have reached the end
     // of the matrice, return 1 to stop recursive.
     if (row == N - 1 && col == N)
         return 1;
- 
+
     //  Check if column reaches the end,
     //  we move to next row and
     //  column start from 0
@@ -70,16 +71,16 @@ int solveSudoku(int grid[N][N], int row, int col)
         row++;
         col = 0;
     }
-   
+
     // Check if the current position
     // of the grid already contains
     // a value (0 for empty) we iterate for next column
     if (grid[row][col] > 0)
         return solveSudoku(grid, row, col + 1);
- 
+
     for (int num = 1; num <= N; num++)
     {
-         
+
         // Check if it is safe to place
         // the num (1-9)  in the
         // given row ,col  ->we move to next column
@@ -87,12 +88,11 @@ int solveSudoku(int grid[N][N], int row, int col)
         {
            // As it's safe we place the num in the empty spot
             grid[row][col] = num;
-           
+
             //  Recursive call, it returns 1 when the sudoku is solved
             if (solveSudoku(grid, row, col + 1)==1)
                 return 1;
         }
-       
         // if it isn't "safe" then we remove the value and place
         // and place the next one we'll get with the loop
         grid[row][col] = 0;
@@ -107,12 +107,15 @@ int main(int argc, char *argv[])
  FILE *fp;
  char *filename;
  char ch;
- int row;
- int col;
- int grid[9][9];
- 
+ long row = 0;
+ long col = 0;
+ long (*grid)[9] = malloc(sizeof(long[9][9]));
+
+    if (argc < 2){
+        printf("To few args");
+        return 1;}
+
    filename = argv[1];
-  
 
    // Open file in read-only mode
    fp = fopen(filename,"r");
@@ -122,7 +125,6 @@ int main(int argc, char *argv[])
    // a int matrice.
    if ( fp )
       {
-        
            while ( (ch = fgetc(fp)) != EOF ) // EOF is End Of File
            {
 
@@ -130,31 +132,34 @@ int main(int argc, char *argv[])
 			   row = 0;
 			   col++;
 		   }
+
 		   if (isdigit(ch)){ // check if number (1-9)
 			   grid[col][row]= ch - '0';
 		           row++;
 		   }
-		   if(ch == '.'){ // check if empty spot
+
+           else if(ch == '.'){ // check if empty spot
 			   grid[col][row]=0;
 			   row++;
 		   }
-                
+
+
            }
-         if (solveSudoku(grid, 0, 0)==1) // if the sudoku has been solved
-             print(grid); // We write it into the result file
-         else { 
+         if (solveSudoku(grid, 0, 0)){ // if the sudoku has been solved
+             print(grid);} // We write it into the result file
+         else {
              printf("Unsolvable sudoku");
-             return 1;}
-             
+             return 1;
+         }
         }
-       
    else
       {
          printf("Failed to open the file\n");
+         return 1;
         }
-         
 
-return(0);
+fclose(fp);
+return 0;
 }
 
 
