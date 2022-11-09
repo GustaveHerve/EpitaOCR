1a804ce78535666890085aad5f45e38bd126f1f8
Gustave HERVE <gustave.herve@epita.fr>
2022-11-09 13:59:23 +0100

    rotation to main

--- include/rotate_img.h
+++ include/rotate_img.h
@@ -4,5 +4,6 @@
 #define ROTATE_H
 
 void rotate_img(SDL_Surface *img, double angle);
+void rotate_img90(SDL_Surface *img, double angle);
 
 #endif
--- main.c
+++ main.c
@@ -11,8 +11,25 @@ int main (int argc, char* argv[])
 {
 
     init_sdl();
-    SDL_Surface *img = load_image("/Users/gustave/Documents/c/images/image_03.jpeg");
-    rotate_img(img, M_PI); //atof converts a "string" to a double
+    SDL_Surface *img = load_image(argv[1]);
+    double angle = atof(argv[2]);
+    if(angle == 360)
+    {
+        open_img_window(argv[1]);
+	return 0;
+    }
+    else
+    {
+        if (angle > 180)
+		angle = -(360-angle);
+//        if (angle == 90 || angle == 180)
+            rotate_img90(img, angle);
+//        else
+//            rotate_img(img, angle); //atof converts a "string" to a double
+    }
     open_img_window("image_rotated.png");
-
+    SDL_FreeSurface(img);
+    IMG_Quit();
+    SDL_Quit();
+    return 0;
 }
