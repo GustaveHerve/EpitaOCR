7b52c35768630066432ba8190f7618a3080e70a6
BomboCato <briac.guellec@gmail.com>
2022-12-03 23:49:29 +0100

    Homographic Transform finally functional !

--- include/rotate_img.h
+++ include/rotate_img.h
@@ -9,7 +9,7 @@
 void rotate_img(SDL_Surface *img, double angle);
 void rotate_img90(SDL_Surface *img, double angle);
 double* get_hMatrix(double* old, double* n);
-void homographic_Transform(SDL_Surface *img, Square corners);
+Square homographic_Transform(SDL_Surface *img, Square corners);
 void homographicT(SDL_Surface *img, Square corners);
 
 #endif
